c707f4cff95eab7e992a3206408eddd8fe313352
ErZLy <151209359+ErZLy@users.noreply.github.com>
2023-12-06 00:54:03 -0600

    script-Emiliano-Ramirez.js

--- script-Emiliano-Ramirez.js
+++ script-Emiliano-Ramirez.js
@@ -4,8 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
         var edad = document.getElementById('ayala').value;
         var sexo = document.getElementById('sexo').value;
         var educacion = document.getElementById('educacion').value;
+        var ocupacion = document.getElementById('ocupacion').value; 
+        
         var url = 'respuesta-Emiliano-Ramirez.html?edad=' + encodeURIComponent(edad) + '&sexo=' +
-        encodeURIComponent(sexo) + '&educacion=' + encodeURIComponent(educacion);
+        encodeURIComponent(sexo) + '&educacion=' + encodeURIComponent(educacion) + '&ocupacion=' + encodeURIComponent(ocupacion);
+        
         window.location.href = url;
     });
 });
